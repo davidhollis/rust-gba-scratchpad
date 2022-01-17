fb6bf0675f703549ef8583649a6a58d728e5a432
David Hollis <david@davidhollis.net>
2022-01-17 14:10:14 -0800

    Populate rom header fields

--- build.sh
+++ build.sh
@@ -7,7 +7,13 @@ cargo build --release
 mkdir -p target/gba
 for product in buttons running; do
   rom_path="target/gba/${product}.gba"
+  # rom title is the UPPERCASE product name padded or clipped to 12 bytes
+  rom_title="$(printf "%-12.12s" "$(echo $product | tr '[:lower:]' '[:upper:]')")"
+  # rom code is the UPPERCASE first two characters of the product name with circumfix C__E
+  rom_code="$(printf "C%-2.2sE" "$(echo $product | tr '[:lower:]' '[:upper:]')")"
+  rom_maker='DA'
+  rom_version='0'
   arm-none-eabi-objcopy -O binary "target/thumbv4t-none-eabi/release/${product}" "$rom_path"
-  gbafix "$rom_path"
-  echo "> Created $rom_path"
+  gbafix -t"$rom_title" -c"$rom_code" -m"$rom_maker" -r"$rom_version" "$rom_path"
+  echo "> Created $rom_path (AGB-$rom_code $rom_title)"
 done
